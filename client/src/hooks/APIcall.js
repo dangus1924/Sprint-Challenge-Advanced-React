4e9dde6577b7f7a982ef65af27e592c2f386b189
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-12 13:03:44 -0400

    creatd axios call hook

--- client/src/hooks/APIcall.js
+++ client/src/hooks/APIcall.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import axios from './axios';
+
+function getAxios() {
+    const [value, setValue] = useState([])
+
+    useEffect(() => {
+        axios
+        .get('http://localhost:5000/api/players')
+        .then(res => setValue(res.data))
+        .catch(err => console.log('error:', err))
+    })
+    return [value, setValue]
+    
+}
+ export default getAxios;
